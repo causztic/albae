1b22b43adb22da3f2e174adb55bc806cde88d29e
yaojie <limyaojie93@gmail.com>
2017-03-29 10:53:31 +0800

    comment out SM

--- 2dp1.py
+++ 2dp1.py
@@ -6,17 +6,19 @@ GPIO.setmode(GPIO.BCM)
 tempswitch = 4
 optimal = 30
 GPIO.setup(tempswitch, GPIO.OUT)
+while (True):
+    print GPIO.input(tempswitch)
 
 
-class MySMClass(sm.SM):
-    startState = "nice"
+# class MySMClass(sm.SM):
+#     startState = "nice"
 
-    def getNextValues(self, state, inp):
-        temperature = GPIO.input(tempswitch)
-        print temperature
-        nextState = "nice"
-        if temperature > optimal:
-            nextState = "hot"
-        elif temperature < optimal:
-            nextState = "cold"
-        return nextState, ""
+#     def getNextValues(self, state, inp):
+#         temperature = GPIO.input(tempswitch)
+#         print temperature
+#         nextState = "nice"
+#         if temperature > optimal:
+#             nextState = "hot"
+#         elif temperature < optimal:
+#             nextState = "cold"
+#         return nextState, ""

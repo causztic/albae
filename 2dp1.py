5b9243dc991b57b40810fcb443078d8d4f9e889d
yaojie <limyaojie93@gmail.com>
2017-03-29 11:07:19 +0800

    update with statemachine

--- 2dp1.py
+++ 2dp1.py
@@ -1,24 +1,55 @@
-import libdw.sm as sm
-import RPi.GPIO as GPIO
+import os
+import glob
+import time
 
-GPIO.setmode(GPIO.BCM)
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
 
-tempswitch = 4
-optimal = 30
-GPIO.setup(tempswitch, GPIO.OUT)
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+optimal = 27
+
+
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos + 2:]
+        temp_c = float(temp_string) / 1000.0
+        # temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c
+
+
+class TemperatureSM(sm.SM):
+    startState = "nice"
+
+    def getNextValues(self, state, inp):
+        temperature = read_temp()
+        power = 0.5
+
+        nextState = "nice"
+        if temperature > optimal:
+            nextState = "hot"
+        elif temperature < optimal:
+            nextState = "cold"
+            power = 0
+        print temperature, nextState
+        return nextState, (power, power)
+
+tsm = TemperatureSM()
+
 while (True):
-    print GPIO.input(tempswitch)
-
-
-# class MySMClass(sm.SM):
-#     startState = "nice"
-
-#     def getNextValues(self, state, inp):
-#         temperature = GPIO.input(tempswitch)
-#         print temperature
-#         nextState = "nice"
-#         if temperature > optimal:
-#             nextState = "hot"
-#         elif temperature < optimal:
-#             nextState = "cold"
-#         return nextState, ""
+    tsm.step("")
+    time.sleep(1)

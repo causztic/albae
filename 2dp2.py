cfb56ee8cf5bd20621b5d1956785da7aa8d26d1b
yaojie <limyaojie93@gmail.com>
2017-04-07 10:37:45 +0800

    + other components

--- 2dp2.py
+++ 2dp2.py
@@ -2,11 +2,14 @@ import os
 import glob
 import time
 
+from functools import partial
 from libdw import sm
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.clock import Clock
+
 # import RPi.GPIO as GPIO
 
 # GPIO.setmode(GPIO.BCM)
@@ -24,34 +27,38 @@ from kivy.uix.textinput import TextInput
 # GPIO.setup(FAN_1, GPIO.OUT)
 # GPIO.setup(FAN_2, GPIO_OUT)
 
-# os.system('modprobe w1-gpio')
-# os.system('modprobe w1-therm')
+use_thermometer = True
 
-# base_dir = '/sys/bus/w1/devices/'
-# device_folder = glob.glob(base_dir + '28*')[0]
-# device_file = device_folder + '/w1_slave'
+try:
+    os.system('modprobe w1-gpio')
+    os.system('modprobe w1-therm')
 
-# optimal = 27
-
-
-# def read_temp_raw():
-#     f = open(device_file, 'r')
-#     lines = f.readlines()
-#     f.close()
-#     return lines
+    base_dir = '/sys/bus/w1/devices/'
+    device_folder = glob.glob(base_dir + '28*')[0]
+    device_file = device_folder + '/w1_slave'
+except:
+    print "Thermometer not detected!"
+    use_thermometer = False
 
 
-# def read_temp():
-#     lines = read_temp_raw()
-#     while lines[0].strip()[-3:] != 'YES':
-#         time.sleep(0.2)
-#         lines = read_temp_raw()
-#     equals_pos = lines[1].find('t=')
-#     if equals_pos != -1:
-#         temp_string = lines[1][equals_pos + 2:]
-#         temp_c = float(temp_string) / 1000.0
-#         # temp_f = temp_c * 9.0 / 5.0 + 32.0
-#         return temp_c
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos + 2:]
+        temp_c = float(temp_string) / 1000.0
+        # temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c
 
 
 # def setWaterPumpAndFan():
@@ -71,32 +78,77 @@ class TemperatureSM(sm.SM):
 
     startState = "cold"
     k = 1
-    optimal = 27
+    optimal = 27.0
+
+    def __init__(self):
+        self.state = self.startState
+
     def getNextValues(self, state, inp):
         power = 1.0
         nextState = "cold"
-        if inp > self.optimal:
+
+        if float(inp) > self.optimal:
             nextState = "hot"
+        elif float(inp) < self.optimal:
+            nextState = "cold"
 
         if state == "hot":
             power = 1.0
         elif state == "cold":
             power = 0
-
         return nextState, (power, power)
 
-class MainWindow(GridLayout):
-    pass
 
 class AlbaeApp(App):
 
+    def system_temp_change(self, instance, value):
+        self.updateGUI(value)
+
+    def __init__(self, **kwargs):
+        App.__init__(self, **kwargs)
+        self.tsm = TemperatureSM()
+        self.target = TextInput(text=str(self.tsm.optimal))
+        self.system_temp = TextInput(text=str(25.0))
+        self.system_temp.bind(text=self.system_temp_change)
+
+        self.surr_temp = Label(
+            text=read_temp() if use_thermometer else "Not detected")
+        self.fp = Label(text="0.0")
+        self.wpp = Label(text="0.0")
+
     def build(self):
-        tsm = TemperatureSM()
-        main = GridLayout(col=2)
+
+        main = GridLayout(cols=2)
+
         main.add_widget(Label(text="Target Temperature in Celsius"))
-        target = TextInput()
-        main.add_widget(target)
+        main.add_widget(self.target)
+
+        main.add_widget(Label(text="Change the System Temperature"))
+        main.add_widget(self.system_temp)
+
+        main.add_widget(
+            Label(text="Algae Temperature \n (if thermometer is detected)", halign="center"))
+        main.add_widget(self.surr_temp)
+
+        main.add_widget(Label(text="Fan Power"))
+        main.add_widget(self.fp)
+
+        main.add_widget(Label(text="Water Pump Power"))
+        main.add_widget(self.wpp)
+        if use_thermometer:
+            Clock.schedule_interval(self.updateGUI(), 1)
         return main
+
+    def updateGUI(self, temp=None):
+        if not temp:
+            temp = read_temp()
+        fan_power, wp_power = self.tsm.step(temp)
+        self.fp.text = str(fan_power)
+        self.wpp.text = str(wp_power)
+
+        if use_thermometer:
+            self.surr_temp = str(temp)
+
 
 if __name__ == '__main__':
     AlbaeApp(name="Albae").run()
